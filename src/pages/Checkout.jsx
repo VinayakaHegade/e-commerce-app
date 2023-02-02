038d0e8118423da695836798f25310cc4cbbd66f
Vinayak <myselfvinayakhegde@gmail.com>
2023-02-02 14:35:36 +0530

    Checkout Stepper Layout done

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Toolbar from "@mui/material/Toolbar";
+import Paper from "@mui/material/Paper";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
 
+const steps = ["Shipping Address", "Payment Details", "Review Order"];
 export default function Checkout() {
+  const [activeStep, setActiveStep] = useState(0);
   return (
-    <div>Checkout</div>
-  )
+    <Container component={"section"} maxWidth="lg" sx={{ mb: 4 }}>
+      <Paper
+        variant="outlined"
+        sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+      >
+        <Typography component={"h1"} variant="h4" align="center">
+          Checkout
+          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+            {steps.map((step) => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </Typography>
+      </Paper>
+    </Container>
+  );
 }
